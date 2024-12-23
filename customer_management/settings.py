566a10880934058b9cd56f5ea05c4636909c656f
Brianonyango879 <brianonyango879@gmail.com>
2024-12-23 08:23:24 +0300

    updated customer management system

--- customer_management/settings.py
+++ customer_management/settings.py
@@ -122,7 +122,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 import os
 from pathlib import Path
-from django.contrib import staticfiles
+
 
 
 BASE_DIR = Path(__file__).resolve().parent.parent
